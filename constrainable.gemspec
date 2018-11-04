3c8ea22ebe9c5d9928ac9399577329dca8308ff3
Ernie Brodeur <ebrodeur@ujami.net>
2018-11-04 11:38:27 -0800

    outlined notes

--- constrainable.gemspec
+++ constrainable.gemspec
@@ -1,5 +1,4 @@
-
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require "constrainable/version"
 
@@ -13,14 +12,26 @@ Gem::Specification.new do |spec|
   spec.description   = "."
   spec.homepage      = "https://github.com/erniebrodeur/constrainable"
 
-  spec.files         = Dir.chdir(File.expand_path('..', __FILE__)) do
+  spec.files         = Dir.chdir(File.expand_path(__dir__)) do
     `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
   end
   spec.bindir        = "exe"
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'bump'
+  spec.add_development_dependency 'bundler'
+  spec.add_development_dependency 'guard'
+  spec.add_development_dependency 'guard-bundler'
+  spec.add_development_dependency 'guard-rspec'
+  spec.add_development_dependency 'guard-rubocop'
+  spec.add_development_dependency 'guard-yard'
+  spec.add_development_dependency 'pry'
+  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rspec'
+  spec.add_development_dependency 'rubocop'
+  spec.add_development_dependency 'rubocop-rspec'
+  spec.add_development_dependency 'simplecov'
+  spec.add_development_dependency 'simplecov-console'
+  spec.add_development_dependency 'yard'
 end
--- lib/constrainable.rb
+++ lib/constrainable.rb
@@ -1,5 +1,89 @@
 require "constrainable/version"
 
 module Constrainable
-  # Your code goes here...
+  # def constrain(*args); end
+
+  # generate a 'named' method as part of the dsl (to supply required_keyword below).
+  # attach that method to Constrainable
+  # by including Constrainable (or Constrainable::ClassMethods or whatever) you will end up with a new
+  # method called required_keyword
+  module_function
+  def required_keyword(binding: binding, arguments: arguments)
+    raise 'AbstractClass'
+  end
+end
+
+# abstract
+class Constraint
+  def generate_named_method
+  end
+
+  def call(binding: nil, arguments: nil)
+  end
+end
+
+class RequiredKeyword < Constraint
+  def method_name
+    :required_keyword # get this automagically (in the future)
+  end
+
+  def call(binding: nil, arguments: nil)
+  # figure out how to pass the binding
+  # dig and find the local bit from the binding.
+  raise ArgumentError, "#{keyword} was not supplied" unless value
+end
+
+class A
+  include Constrainable
+
+  @constraints = {
+    RequiredArgument => %i[keyword two three]
+  }
+
+  # later on
+  constraints.each {|c, arguments| c.call(binding: binding, arguments: arguments) }
+
+  # begin
+  #   constrain :some_method, :arg1, kind_of: String, required: true
+  #   constrain :some_method, :arg2, kind_of: String, required: true
+  #   constrain :some_method, :two, kind_of: String, required: true
+  #   constrain :some_method, :three, kind_of: String, required: true
+  # end
+
+  # by :required_keywords, :some_method, kind_of: %i[keyword two three]
+  # by :user_signed_in, :some_method, user: user
+
+  # Constrainable.by_require_keywords :some_method, %i[keyword two three]
+  # Constrainable.by_kind_of :some_method, %i[keyword two three], [String, nil]
+
+  # by_user_signed_in :some_method, user
+  # by_require_keywords %i[keyword two three]
+  # by_kind_of [String, nil], %i[keyword two three]
+  # by_kind_of Array, %i[arg1 arg2]
+  # this call will supply the binding and the method_name (since it's in scope).
+  
+  def some_method(arg1, arg2, keyword: nil, two: nil, three: nil)
+    raise ArgumentError, 'keyword must be present' unless keyword
+    raise ArgumentError, 'two must be present' unless two
+    raise ArgumentError, 'three must be present' unless three
+    # -> factors to this
+    [keyword, two, three].each { |v| raise ArgumentError, "#{v} must be present" unless v }
+    # -> factors to this
+    required_keyword %i[keyword two three]
+
+    puts arg1, arg2, keyword, two, three
+  end
+end
+
+module Constrainable
+  module By
+    module UserSignedIn
+      module_function
+      def wrapper(method, *args)
+        puts 'blow_up' unless args.any?
+
+        send method, args
+      end
+    end
+  end
 end
